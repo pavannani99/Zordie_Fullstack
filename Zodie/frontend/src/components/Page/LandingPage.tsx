f59c17d3479fdd902fddfeb1cb5c27117ae93236
Snehal Srivastava <118104081+SnehalSrivastava27@users.noreply.github.com>
2025-06-15 20:31:05 +0530

    Home

--- Zodie/frontend/src/components/Page/LandingPage.tsx
+++ Zodie/frontend/src/components/Page/LandingPage.tsx
@@ -6,12 +6,14 @@ import { Faq3Demo } from '@/demo/faqdemo'
 import { Footerdemo } from '@/demo/fs'
 import Flow from '../blocks/Flow'
 import HRAutomationSection from '../blocks/Analytics'
+import Productivity from '../blocks/Productivity'
 export const LandingPage=()=>{
     return(
         <div className='relative bg-[radial-gradient(120%_120%_at_50%_100%,rgba(253,186,116,0.8)_0%,white_85%)]'>
         <HeroSection></HeroSection>
          <HRAutomationSection></HRAutomationSection>
         <Flow></Flow>
+        <Productivity></Productivity>
        
         {/* <AgentInfo></AgentInfo> */}
         {/* <Home></Home> */}
